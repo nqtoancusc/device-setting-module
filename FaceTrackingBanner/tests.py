69255a5036b5da0b4fb3c3de750049b857751d8e
toan nguyen <toannguyen@toans-MacBook-Pro.local>
2020-09-03 23:21:02 +0300

    Add a test case for api setting

--- FaceTrackingBanner/tests.py
+++ FaceTrackingBanner/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+class ApiTestCase(APITestCase):
+    def test_setting_api_return_empty(self):
+        """
+        Ensure that the response empty list if device_id does not exist.
+        """
+        url = "/api/v1/setting/?device_id=1012"
+
+        dataResponse = []
+        
+        response = self.client.get(url)
+        self.assertEqual(json.loads(response.content), dataResponse)
